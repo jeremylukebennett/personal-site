3c64410564e5cbbe2bb6a0c55098e5dcc9dcb289
Jeremy Bennett <jeremylukebennett@comcast.net>
2018-02-01 15:59:53 -0600

    Used javascript to display blogs

--- main.js
+++
@@ -1,7 +0,0 @@
-var myIntroButton = document.getElementById("js-btn");
-
-
-
-myIntroButton.addEventListener("click", () => {
-    document.getElementById("btn-response").innerHTML = "I did a JavaScript! Now check out my <a href=blog.html>blog</a> while I work on a real homepage! ¯\\\_(ツ)_/¯";
-});
